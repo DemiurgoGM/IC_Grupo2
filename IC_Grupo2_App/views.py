9f0526b17167fd6f95527cedb7600dc0bd3ac6b1
Vinícius Barros <vinicius.oliveira@liferay.com>
2021-04-24 23:39:41 -0300

    FT-1 | Return zipfile as HTTP response

--- IC_Grupo2_App/views.py
+++ IC_Grupo2_App/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 from django.conf import settings
+
+import zipfile 
 
 import os
 
@@ -25,11 +28,23 @@ def send(request):
     path = default_storage.save('tmp/input_file.pdf', ContentFile(input_file.read()))
     tmp_file = os.path.join(settings.MEDIA_ROOT, path)
  
-    highlighted_pdf, csv = ApplicationFactory().get_application_instance(tmp_file, endereco).run()
+    highlighted_pdf, extracted_data_csv = ApplicationFactory().get_application_instance(tmp_file, endereco).run()
 
-    # response = HttpResponse(file, headers={
-    #     'Content-Type': 'application/zip',
-    #     'Content-Disposition': 'attachment; filename="foo.zip"',
-    # })
+    zip_file = zipfile.ZipFile('tmp/retorno.zip', 'w')
+    
+    zip_file.write('tmp/highlighted.pdf')
+    zip_file.write('tmp/extracted_data.csv')
+    
+    zip_file.close()
+    
+    response = HttpResponse(open('tmp/retorno.zip', 'rb'), headers = {
+        'Content-Type': 'application/zip',
+        'Content-Disposition': 'attachment; filename="resultado.zip"',
+    })
+
+    default_storage.delete('tmp/retorno.zip')
+    default_storage.delete('tmp/input_file.pdf')
+    default_storage.delete('tmp/highlighted.pdf')
+    default_storage.delete('tmp/extracted_data.csv')
 
     return response
